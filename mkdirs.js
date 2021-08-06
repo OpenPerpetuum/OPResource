bb5bb9559987bd945c96aa0c086e687df905cbda
mikejeffers <mike.s.jeffers@gmail.com>
2021-08-05 21:47:15 -0400

    script to make folder structure

---
+++ mkdirs.js
@@ -0,0 +1,15 @@
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const path = require('path');
+console.log(__dirname);
+const startZoneId = 100;
+const endZoneId = 141;
+const layerTypes = ['altitude', 'blocks', 'control', 'plants'];
+const gitignore = '*\n!.gitignore';
+for(const layerType of layerTypes){
+    for(let i=startZoneId; i<endZoneId; i++){
+        const folder = path.join(__dirname, `lang0000`, `layers`, `${i}`, `${layerType}${i}`).toString();
+        mkdirp.sync(folder);
+        fs.writeFileSync(path.join(folder, '.gitignore'), gitignore);
+    }
+}
