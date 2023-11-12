de04fd12ed27f2b8ba58dce3437f0b0238bbfcbb
AqpAnaciy <101519462+aqpanaciy@users.noreply.github.com>
2023-11-12 08:38:49 +0300

    Add fallthrough in index.js
    - add default.png for icons;
    - add GAMMA_LAYERS_NEW dir for compress on the fly;
    - update index.js to use raw layer data and default icon;
    - update README.md .

--- README.md
+++ README.md
@@ -3,8 +3,15 @@
 # OPResource
 
 ## Quick start
+
 With Node.Js:
+
+1.  Install [Node.js](https://nodejs.org/) .
+1.  Setup folder *lang0000\layers\GAMMA_LAYERS_NEW* content (See README.md in directory).
+2.  Add string in server config file (perpetuum.ini): *"ResourceServerURL": "http://127.0.0.1:1337/",*
+3.  At the command prompt, enter:
 ```sh
+npm i
 node index.js
 ```
 Or see Documentation
--- index.js
+++ index.js
@@ -7,8 +7,13 @@
 
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
+const stream = require('stream');
+const gzip = require('zlib');
+
 const app = express();
-const port = 80;
+const port = process.env.PORT || 1337;
+const layerspath = process.env.GAMMA_LAYERS_NEW || 'lang0000/layers/GAMMA_LAYERS_NEW'
 
 // Log on any file access
 app.use(function(req, res, next){
@@ -16,12 +21,79 @@ app.use(function(req, res, next){
     next();
 });
 
-app.use('/lang0000', express.static('lang0000'));
+// By default, we give resources along the request path
+app.use('/lang0000', express.static('lang0000', { fallthrough: true }));
+
+// Replace the not found images with the default.
+app.use('/**/*.png', express.static('lang0000/icons/default/00000000.png'));
+
+// Сompress the file, add its size to the beginning and send it as an answer.
+function DeflateAndSeend(file, responce) {
+    // Four bytes with the size of the source file.
+    let buf = Buffer.alloc(4);
+    buf.writeInt32LE(fs.statSync(file).size)
+    responce.write(buf);
+
+    // Pipeline for delivering compressed content to the client.
+    console.log('Defalte and send file:', file);
+    stream.pipeline(fs.createReadStream(file), gzip.createDeflate(), responce, (err) => {
+        if (err) {
+            console.log('Pipeline error:', err, 'in', file);
+            return responce.status(400).send();
+        }
+    });
+}
+
+// Prepare and deliver layer files from raw files intended for the server.
+function ProcessReq(layer, request, responce)
+{
+    // Looking for the zone number in the request.
+    const url = request.url.match(/\/\d{4}\//);
+    if (url == null) {
+        console.log('No zone number');
+        return responce.status(400).send();
+    }
+    const zone = url[0].match(/\d{4}/)[0];
+
+    // Сheck that the request matches the template.
+    const expect = new RegExp('\\/lang0000\\/layers\\/' + zone + '\\/' + layer + zone + '\\/\\d{8}\\.dat');
+    if (expect.exec(request.url) == null) {
+        console.log('URL format incorrect');
+        return responce.status(400).send();
+    }
+
+    // Collecting the path to the file with raw data.
+    const bin = path.join(__dirname, layerspath, layer + '.' + zone + '.bin');
+    console.log('Client hit', layer, bin);
+
+    // This should compress and transfer the file on the fly.
+    DeflateAndSeend(bin, responce);
+}
+
+// Thes altitude layer request.
+app.get('/lang0000/layers/*/altitude*/*.dat', function (req, res) {
+    ProcessReq('altitude', req, res)
+});
+
+// The blocks layer request.
+app.get('/lang0000/layers/*/blocks*/*.dat', function (req, res) {
+    ProcessReq('blocks', req, res)
+});
+
+// The altitude layer request.
+app.get('/lang0000/layers/*/control*/*.dat', function (req, res) {
+    ProcessReq('control', req, res)
+});
+
+// The plants layer request.
+app.get('/lang0000/layers/*/plants*/*.dat', function (req, res) {
+    ProcessReq('plants', req, res)
+});
 
 // The index
 app.get('/', function(req, res, next) {
-    console.log("Client hit index!");
-    res.sendFile(path.join(__dirname + '/resource_0.dat'));
+    console.log('Client hit index!');
+    res.sendFile(path.join(__dirname, 'resource_0.dat'));
 });
 
-app.listen(port, () => console.log(`Local development OPResource server initialized`));
+app.listen(port, () => console.log('Local development OPResource server initialized'));
