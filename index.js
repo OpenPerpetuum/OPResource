8ec6a9d0e65c8ded889dceded7612ef116d5c412
AqpAnaciy <101519462+aqpanaciy@users.noreply.github.com>
2025-11-21 20:13:53 +0300

    High resolution textures for the ground surface (#92)
    * Process requests only from the game client
    
    * Check file availability
    
    * Fix TAB
    
    * Update 00000001.txt
    
    * High resolution textures for the ground surface

--- index.js
+++ index.js
@@ -17,8 +17,15 @@ const layerspath = process.env.GAMMA_LAYERS_NEW || 'lang0000/layers/GAMMA_LAYERS
 
 // Log on any file access
 app.use(function(req, res, next) {
-    console.log(req.originalUrl);
-    next();
+    let agent = req.headers['user-agent'];
+    if (!agent || !agent.match(/Perpetuum/)) {
+        res.status(403).send();
+    }
+    else
+    {
+        console.log(req.originalUrl);
+        next();
+    }
 });
 
 // By default, we give resources along the request path
@@ -29,6 +36,12 @@ app.use('/**/*.png', express.static('lang0000/icons/default/00000000.png'));
 
 // Сompress the file, add its size to the beginning and send it as an answer.
 function DeflateAndSeend(file, responce) {
+    // Check file availability.
+    if (!fs.existsSync(file)) {
+        console.log('File does not exist:', file);
+        return responce.status(400).send();
+    }
+
     // Four bytes with the size of the source file.
     let buf = Buffer.alloc(4);
     buf.writeInt32LE(fs.statSync(file).size);
@@ -93,7 +106,13 @@ app.get('/lang0000/layers/*/plants*/*.dat', function (req, res) {
 // The index
 app.get('/', function(req, res, next) {
     console.log('Client hit index!');
-    res.sendFile(path.join(__dirname, 'resource_0.dat'));
+    // Check file availability.
+    let index = path.join(__dirname, 'resource_0.dat')
+    if (!fs.existsSync(index)) {
+        console.log('File does not exist:', index);
+        return res.status(400).send();
+    }
+    res.sendFile(index);
 });
 
 app.listen(port, () => console.log('Local development OPResource server initialized'));
